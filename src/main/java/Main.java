8f89ba9315a57817bb2f733a35201638fc80936b
Nou7en <127136593+Nou7en@users.noreply.github.com>
2025-06-24 19:44:26 +0000

    feat:Nueva funcionalidad despedida

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,11 @@
 public class Main {
     public static void main(String[] args) {
+        // Saludo original
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Mundo"));
+
+        // Nueva funcionalidad de despedida
+        Despedida despedida = new Despedida();
+        System.out.println(despedida.generarDespedida("Mundo"));
     }
-}gi
+}
