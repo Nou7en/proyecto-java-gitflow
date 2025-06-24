e224baf46299dff757fb2366b6b07e8fc6f9d321
Nou7en <127136593+Nou7en@users.noreply.github.com>
2025-06-24 19:57:43 +0000

    feat:Nuevo Logo del proyecto

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,15 @@
 public class Main {
     public static void main(String[] args) {
+        // Logo ASCII
+        System.out.println("***************");
+        System.out.println("* JAVA PROJECT *");
+        System.out.println("***************");
+
         // Saludo original
         Saludo saludo = new Saludo();
         System.out.println(saludo.generarSaludo("Mundo"));
 
-        // Nueva funcionalidad de despedida
+        // Funcionalidad de despedida
         Despedida despedida = new Despedida();
         System.out.println(despedida.generarDespedida("Mundo"));
     }
