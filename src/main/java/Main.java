3edd0f53b3928c988df351d39320395afa1b0ba3
Nou7en <127136593+Nou7en@users.noreply.github.com>
2025-06-24 20:06:27 +0000

    feat:Nuevos saludos multilingues

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,17 @@
 public class Main {
     public static void main(String[] args) {
-        // Saludo original
-        Saludo saludo = new Saludo();
-        System.out.println(saludo.generarSaludo("Mundo"));
+        // Logo ASCII
+        System.out.println("***************");
+        System.out.println("* JAVA PROJECT *");
+        System.out.println("***************");
 
-        // Nueva funcionalidad de despedida
+        // Saludo multilingüe
+        Saludo saludo = new Saludo();
+        System.out.println(saludo.generarSaludo("Mundo", "es"));
+        System.out.println(saludo.generarSaludo("World", "en"));
+        System.out.println(saludo.generarSaludo("Monde", "fr"));
+
+        // Funcionalidad de despedida
         Despedida despedida = new Despedida();
         System.out.println(despedida.generarDespedida("Mundo"));
     }
